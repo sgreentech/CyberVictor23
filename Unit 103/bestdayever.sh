b17568db87e3a86a6c1d1bbaaf9d4e53f4368ff0
Victor <vnieva.w@gmail.com>
2023-11-07 16:40:21 -0800

    20231107

---
+++ Unit 103/bestdayever.sh
@@ -0,0 +1,14 @@
+name=$1
+compliment=$2
+
+user=$(whoami)
+date=$(date)
+whereami=$(pwd)
+
+echo "Hello $name!"
+sleep 1
+echo "Youre looking fantastic $name!"
+sleep 1
+echo "Perhaps you could join the $compliment!"
+sleep 2
+echo "You here as $user. On this day that is $date. You are currently within $whereami"
