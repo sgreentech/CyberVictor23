2f2049d31f95cc157aa399a58cfe8b053264804e
Victor <vnieva.w@gmail.com>
2023-11-08 01:54:16 -0800

    20231108

---
+++ Unit 103/ops3.sh
@@ -0,0 +1,8 @@
+echo "Enter a number"
+read number
+if (($number > 5 ))
+    then echo "Thats more than 5 bud!"
+elif (($number < 5))
+    then echo "Pfft that's smaller than 5!"
+else echo "We have ourselves a 5 I reckon!"
+fi
