fbce9db8ad593f17616c8e2723f4d2fce4cec690
Victor <vnieva.w@gmail.com>
2023-11-08 16:20:55 -0800

    20231108

--- Unit 103/ops3.sh
+++ Unit 103/ops3.sh
@@ -1,8 +1,8 @@
 echo "Enter a number"
 read number
-if (($number > 5 ))
-    then echo "Thats more than 5 bud!"
-elif (($number < 5))
-    then echo "Pfft that's smaller than 5!"
-else echo "We have ourselves a 5 I reckon!"
+if (($number > $RANDOM ))
+    then echo "Thats more than $RANDOM bud!"
+elif (($number < $RANDOM))
+    then echo "Pfft that's smaller than $RANDOM!"
+else echo "We have ourselves a $RANDOM I reckon!"
 fi
