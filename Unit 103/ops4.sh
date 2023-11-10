7728f59a112dc10ed9747e4430b16c501c010a8a
Victor <vnieva.w@gmail.com>
2023-11-09 16:21:50 -0800

    20231109

--- Unit 103/ops4.sh
+++ Unit 103/ops4.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
 
-echo "Enter 2 number to add"
+# echo "Enter 2 number to add"
+# read num1
+# read num2
+# function add(){
+#     sum=$(( $num1 + $num2 ))
+#     echo $sum
+#     }
+#     add
+
+# echo "Enter 2 number to multiply"
+# read num1
+# read num2
+# function multiply(){
+#     sum=$(( $num1 * $num2 ))
+#     echo $sum
+#     }
+#     multiply
+
+echo "Give me 2 numbers to divide bruv!"
 read num1
 read num2
-function add(){
-    sum=$(( $num1 + $num2 ))
+function divide(){
+    sum=$(( $num1 / $num2 ))
     echo $sum
     }
-    add
+    divide
