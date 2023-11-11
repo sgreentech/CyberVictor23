ee8d808090b37c08b7c2fdf6792d18ab60dc533b
Victor <vnieva.w@gmail.com>
2023-11-10 23:50:11 -0800

    20231110

--- Resources/LB.sh
+++ Resources/LB.sh
@@ -51,26 +51,26 @@
 # fi
 
 # #While loop run till acceptance not met
-# user=y
-# while [ $user = y ]
-# do
-# echo "Please make a selection"
-# echo "1 to print hello world" 
-# echo "2 ping yourself"
-# echo "3 to show network config"
-# read input
-#     if [ $input = 1 ]
-#         then echo "Hello World!"
-#     elif [ $input = 2 ]
-#         then ping -c 1 192.168.0.1
-#     elif [ $input = 3 ]
-#         then ifconfig
-#     else echo "Invalid entry"
-# fi
-# echo "do you want to play again: y/n"
-# read user
-# done
-# echo "good work"
+user = y
+while [ $user = y ]
+do
+echo "Please make a selection"
+echo "1 to print hello world" 
+echo "2 ping yourself"
+echo "3 to show network config"
+read input
+    if [ $input = 1 ]
+        then echo "Hello World!"
+    elif [ $input = 2 ]
+        then ping -c 1 192.168.0.1
+    elif [ $input = 3 ]
+        then ifconfig
+    else echo "Invalid entry"
+fi
+echo "do you want to play again: y/n"
+read user
+done
+echo "good work"
 
 # #functions is to be called multiple times by only using function name
 # echo "Enter 2 number to add"
