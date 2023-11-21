c0d9804e161d9df4aaaf525098412849ff559d0b
Victor <vnieva.w@gmail.com>
2023-11-21 01:08:22 -0800

    done

--- Unit 103/g.sh
+++ Unit 103/g.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 #function to auto push changes into git
 
+user=x
+while [ $user=x ]
+do
+read message
 function lazy() {
     git add *
-    git commit -a -m ' $1 '
+    git commit -a -m "$message"
     git push
 }
 lazy
+done
