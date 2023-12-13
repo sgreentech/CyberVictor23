258a3238eb230041fcb3fb3cde7a697308b245f6
victornroctiv <vnieva.w@gmail.com>
2023-12-13 14:57:00 -0800

    ops3

---
+++ Unit 104/ops3.py
@@ -0,0 +1,9 @@
+# Scenario
+# Using one of the comparison operators in Python, write a simple two-line program that takes the parameter n as input,
+#  which is an integer, and prints False if n is less than 100, and True if n is greater than or equal to 100.
+
+n = int(input("Enter a number:"))
+m = 100
+if n >= m:
+    print("True")
+else: print ("False")
