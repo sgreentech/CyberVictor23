ed93146a9932d0a85bdefaf26cf650cdd45eae2b
victornroctiv <vnieva.w@gmail.com>
2023-12-13 10:33:22 -0800

    ops2

--- Unit 104/g.py
+++ Unit 104/g.py
@@ -1,13 +1,19 @@
 #python
 #function to auto push changes into git
 
-user = x
-while ( user = x )
-message = input ("What did you update? ")
-def lazy() {
-    git add *
-    git commit -a -m message
-    git push
-}
-lazy()
-user = input ("Enter c to complete")
+from git import Repo
+
+PATH_OF_GIT_REPO = r"V:\GitHub Reps\CyberBCoct23\Unit 104"  # make sure .git folder is properly configured
+COMMIT_MESSAGE = 'comment from python script'
+
+def git_push():
+    try:
+        repo = Repo(PATH_OF_GIT_REPO)
+        repo.git.add(update=True)
+        repo.index.commit(COMMIT_MESSAGE)
+        origin = repo.remote(name='origin')
+        origin.push()
+    except:
+        print('Some error occured while pushing the code')    
+
+git_push()
---
+++ Unit 104/ops2.py
@@ -0,0 +1,66 @@
+# Objectives
+# Create if statements using these logical conditionals below. Each statement should print information to the screen depending on if the condition is met.
+
+# # Equals: a == b
+# a = 5
+# b = 5
+# if a == b:
+#     print("a is equal to b")
+
+# # Not Equals: a != b
+# a=6
+# b=8
+# if a != b:
+#     print("a is not b")
+
+# # Less than: a < b
+# if a < b:
+#     print("a is afraid of b because b is bigger")
+
+# # Less than or equal to: a <= b
+# if a <= b:
+#     print("a is slightly afraid of b because a could be the same as b")
+
+# # Greater than: a > b
+# a=9
+# b=8
+# if a > b:
+#     print("a grew a pair and is now stronger than b")
+
+# # Greater than or equal to: a >= b
+# if a>= b:
+#     print("a underestimated b, b may be stronger than he looks")
+
+# Create an if statement using a logical conditional of your choice and include elif keyword that executes when other conditions are not met.
+# q=8
+# z=8
+# if q>z:
+#     print("q is a superior letter to z")
+# elif q:z
+# print("they are just letter")
+
+# Create an if statement that includes both elif and else to execute when both if and elif are not met.
+q=8
+z=8
+if q > z:
+    print("q is a superior letter to z")
+elif q < z:
+    print("z is a superior letter to q")
+else:
+    print("why must we compare letters?")
+
+# Stretch Goals (Optional Objectives)
+# Pursue stretch goals if you are a more advanced user or have remaining lab time.
+
+# Create an if statement with two conditions by using and between conditions.
+if q > z:
+    print("q is a superior letter to z")
+elif q < z:
+    print("z is a superior letter to q")
+   
+
+# Create an if statement with two conditions by using or between conditions.
+
+# Create a nested if statement.
+
+# Create an if statement that includes pass to avoid errors.
