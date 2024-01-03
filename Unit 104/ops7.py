b3f66ac07ceed730e81d5d456e882888aebca147
victornroctiv <vnieva.w@gmail.com>
2024-01-02 22:13:24 -0800

    ops7-complete

--- Unit 104/ops7.py
+++ Unit 104/ops7.py
@@ -8,23 +8,26 @@
 # until the number is guessed correctly and the program ENDS.
 # Remeber to import the random function
 #Bonus objective can you put it into a loop to make the game end after 3 turns?
-
 import random
-tries = 0
-nums = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+
+nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
 
 secret_number = random.choice(nums)
 
 print(secret_number)
+tries = 0
+max_tries = 3
 
-user_num = input('You have 3 chances. Can you guess the number between 1 - 20? ')
-while user_num != secret_number:
-    tries += 1
-    user_num = input('Keep Guessing ')
+while tries < max_tries:
+    user_num = int(input('Guess the number between 1 - 20: '))
+
     if user_num == secret_number:
-        print("Wow you got it right in ", (tries), "guesses!")
+        tries += 1
+        print(f"Wow, you got it right in {tries}!")
         break
     else:
-        tries == 3
-        print("You've used all your guesses!")
-        break
+        tries += 1
+        if tries == max_tries:
+            print(f"Sorry, you've used all your tries. The secret number was {secret_number}.")
+        else:
+            print("Keep guessing.")
