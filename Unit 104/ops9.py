4ce48a2bc39dc39aeee2eca270cc9973228c0386
victornroctiv <vnieva.w@gmail.com>
2024-01-04 17:46:18 -0800

    ops9-complete

--- Unit 104/ops9.py
+++ Unit 104/ops9.py
@@ -10,3 +10,11 @@
 
 
 #Write your code below this row 👇
+
+for x in range(1, 100):
+    if x % 3 == 0 and x % 5 == 0:
+        print("FizzBuzz")
+    elif x % 3 == 0:
+        print("Fizz")
+    elif x % 5 == 0:
+        print("Buzz")
